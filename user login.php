7a243f35215f121dbd8872feddd12ccd50248fc2
nikitapatil6268 <nikitapatil12334@gmail.com>
2024-09-08 16:35:56 +0530

    Add files via upload

---
+++ user login.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+require 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE username = ?');
+    $stmt->execute([$username]);
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        header('Location: profile.php');
+        exit();
+    } else {
+        echo 'Invalid credentials.';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form method="post">
+        Username: <input type="text" name="username" required><br>
+        Password: <input type="password" name="password" required><br>
+        <input type="submit" value="Login">
+    </form>
+    <p><a href="index.php">Don't have an account? Register here.</a></p>
+</body>
+</html>
